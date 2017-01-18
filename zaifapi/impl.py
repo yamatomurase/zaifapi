090335aad86e3fe59cbdfe708b36aab616fbb257
taniguchi_akira <dededededaiou2003@yahoo.co.jp>
2017-01-18 17:20:34 +0900

    is_tokenパラメータ対応

--- zaifapi/impl.py
+++ zaifapi/impl.py
@@ -72,6 +72,9 @@ SCHEMA = {
     },
     'limit': {
         'type': 'number'
+    },
+    'is_token': {
+        'type': 'boolean'
     }
 }
 
@@ -81,26 +84,26 @@ class AbsZaifApi(AbsZaifBaseApi):
     _api_domain = 'api.zaif.jp'
 
     def params_pre_processing(self, schema_keys, params):
-        schema = self.__get_schema(schema_keys)
-        self.__validate(schema, params)
-        return self.__edit_params(params)
+        schema = self._get_schema(schema_keys)
+        self._validate(schema, params)
+        return self._edit_params(params)
 
     @classmethod
-    def __get_schema(cls, keys):
+    def _get_schema(cls, keys):
         schema = {}
         for key in keys:
             schema[key] = SCHEMA[key]
         return schema
 
     @classmethod
-    def __edit_params(cls, params):
+    def _edit_params(cls, params):
         if 'from_num' in params:
             params['from'] = params['from_num']
             del (params['from_num'])
         return params
 
     @classmethod
-    def __validate(cls, schema, param):
+    def _validate(cls, schema, param):
         v = cerberus.Validator(schema)
         if v.validate(param):
             return
@@ -108,32 +111,32 @@ class AbsZaifApi(AbsZaifBaseApi):
 
 
 class ZaifPublicApi(AbsZaifApi):
-    __API_URL = '{}://{}/api/1/{}/{}'
+    _API_URL = '{}://{}/api/1/{}/{}'
 
-    def __params_pre_processing(self, currency_pair):
+    def _params_pre_processing(self, currency_pair):
         params = {
             'currency_pair': currency_pair
         }
         super(ZaifPublicApi, self).params_pre_processing(['currency_pair'], params)
 
-    def __execute_api(self, func_name, currency_pair):
-        self.__params_pre_processing(currency_pair)
-        return get_response(self.__API_URL.format(self.get_protocol(), self._api_domain, func_name, currency_pair))
+    def _execute_api(self, func_name, currency_pair):
+        self._params_pre_processing(currency_pair)
+        return get_response(self._API_URL.format(self.get_protocol(), self._api_domain, func_name, currency_pair))
 
     def last_price(self, currency_pair):
-        return self.__execute_api(inspect.currentframe().f_code.co_name, currency_pair)
+        return self._execute_api(inspect.currentframe().f_code.co_name, currency_pair)
 
     def ticker(self, currency_pair):
-        return self.__execute_api(inspect.currentframe().f_code.co_name, currency_pair)
+        return self._execute_api(inspect.currentframe().f_code.co_name, currency_pair)
 
     def trades(self, currency_pair):
-        return self.__execute_api(inspect.currentframe().f_code.co_name, currency_pair)
+        return self._execute_api(inspect.currentframe().f_code.co_name, currency_pair)
 
     def depth(self, currency_pair):
-        return self.__execute_api(inspect.currentframe().f_code.co_name, currency_pair)
+        return self._execute_api(inspect.currentframe().f_code.co_name, currency_pair)
 
     def streaming(self, currency_pair):
-        self.__params_pre_processing(currency_pair)
+        self._params_pre_processing(currency_pair)
         ws = create_connection('ws://api.zaif.jp:8888/stream?currency_pair={}'.format(currency_pair))
         result = ws.recv()
         ws.close()
@@ -141,92 +144,92 @@ class ZaifPublicApi(AbsZaifApi):
 
 
 class AbsZaifPrivateApi(AbsZaifApi):
-    __API_URL = '{}://{}/tapi'
+    _API_URL = '{}://{}/tapi'
 
     @abstractmethod
     def get_header(self, params):
         raise NotImplementedError()
 
     @classmethod
-    def __get_parameter(cls, func_name, params):
+    def _get_parameter(cls, func_name, params):
         params['method'] = func_name
         params['nonce'] = int(time.mktime(datetime.now().timetuple()))
         return urlencode(params)
 
-    def __execute_api(self, func_name, schema_keys=None, params=None):
+    def _execute_api(self, func_name, schema_keys=None, params=None):
         if schema_keys is None:
             schema_keys = []
         if params is None:
             params = {}
         params = self.params_pre_processing(schema_keys, params)
-        params = self.__get_parameter(func_name, params)
+        params = self._get_parameter(func_name, params)
         header = self.get_header(params)
-        res = get_response(self.__API_URL.format(self.get_protocol(), self._api_domain), params, header)
+        res = get_response(self._API_URL.format(self.get_protocol(), self._api_domain), params, header)
         if res['success'] == 0:
             raise Exception(res['error'])
         return res['return']
 
     def get_info(self):
-        return self.__execute_api(inspect.currentframe().f_code.co_name)
+        return self._execute_api(inspect.currentframe().f_code.co_name)
 
     def get_info2(self):
-        return self.__execute_api(inspect.currentframe().f_code.co_name)
+        return self._execute_api(inspect.currentframe().f_code.co_name)
 
     def get_personal_info(self):
-        return self.__execute_api(inspect.currentframe().f_code.co_name)
+        return self._execute_api(inspect.currentframe().f_code.co_name)
 
     def trade_history(self, **kwargs):
-        schema_keys = ['from_num', 'count', 'from_id', 'end_id', 'order', 'since', 'end', 'currency_pair']
-        return self.__execute_api(inspect.currentframe().f_code.co_name, schema_keys, kwargs)
+        schema_keys = ['from_num', 'count', 'from_id', 'end_id', 'order', 'since', 'end', 'currency_pair', 'is_token']
+        return self._execute_api(inspect.currentframe().f_code.co_name, schema_keys, kwargs)
 
     def active_orders(self, **kwargs):
-        schema_keys = ['currency_pair']
-        return self.__execute_api(inspect.currentframe().f_code.co_name, schema_keys, kwargs)
+        schema_keys = ['currency_pair', 'is_token']
+        return self._execute_api(inspect.currentframe().f_code.co_name, schema_keys, kwargs)
 
-    def __inner_history_api(self, func_name, kwargs):
-        schema_keys = ['currency', 'from_num', 'count', 'from_id', 'end_id', 'order', 'since', 'end']
-        return self.__execute_api(func_name, schema_keys, kwargs)
+    def _inner_history_api(self, func_name, kwargs):
+        schema_keys = ['currency', 'from_num', 'count', 'from_id', 'end_id', 'order', 'since', 'end', 'is_token']
+        return self._execute_api(func_name, schema_keys, kwargs)
 
     def withdraw_history(self, **kwargs):
-        return self.__inner_history_api(inspect.currentframe().f_code.co_name, kwargs)
+        return self._inner_history_api(inspect.currentframe().f_code.co_name, kwargs)
 
     def deposit_history(self, **kwargs):
-        return self.__inner_history_api(inspect.currentframe().f_code.co_name, kwargs)
+        return self._inner_history_api(inspect.currentframe().f_code.co_name, kwargs)
 
     def withdraw(self, **kwargs):
-        schema_keys = ['currency', 'address', 'message', 'amount', 'opt_fee']
-        return self.__execute_api(inspect.currentframe().f_code.co_name, schema_keys, kwargs)
+        schema_keys = ['currency', 'address', 'message', 'amount', 'opt_fee', 'is_token']
+        return self._execute_api(inspect.currentframe().f_code.co_name, schema_keys, kwargs)
 
     def cancel_order(self, **kwargs):
-        schema_keys = ['order_id']
-        return self.__execute_api(inspect.currentframe().f_code.co_name, schema_keys, kwargs)
+        schema_keys = ['order_id', 'is_token']
+        return self._execute_api(inspect.currentframe().f_code.co_name, schema_keys, kwargs)
 
     def trade(self, **kwargs):
-        schema_keys = ['currency_pair', 'action', 'price', 'amount', 'limit']
-        return self.__execute_api(inspect.currentframe().f_code.co_name, schema_keys, kwargs)
+        schema_keys = ['currency_pair', 'action', 'price', 'amount', 'limit', 'is_token']
+        return self._execute_api(inspect.currentframe().f_code.co_name, schema_keys, kwargs)
 
 
 class ZaifPrivateApi(AbsZaifPrivateApi):
     def __init__(self, key, secret):
-        self.__key = key
-        self.__secret = secret
+        self._key = key
+        self._secret = secret
         super(ZaifPrivateApi, self).__init__()
 
     def get_header(self, params):
-        signature = hmac.new(bytearray(self.__secret.encode('utf-8')), digestmod=hashlib.sha512)
+        signature = hmac.new(bytearray(self._secret.encode('utf-8')), digestmod=hashlib.sha512)
         signature.update(params.encode('utf-8'))
         return {
-            'key': self.__key,
+            'key': self._key,
             'sign': signature.hexdigest()
         }
 
 
 class ZaifPrivateTokenApi(AbsZaifPrivateApi):
     def __init__(self, token):
-        self.__token = token
+        self._token = token
         super(ZaifPrivateTokenApi, self).__init__()
 
     def get_header(self, params):
         return {
-            'token': self.__token
+            'token': self._token
         }
--- zaifapi/token.py
+++ zaifapi/token.py
@@ -4,29 +4,29 @@ from zaifapi.api_common import get_response, AbsZaifBaseApi
 
 
 class ZaifTokenApi(AbsZaifBaseApi):
-    __API_URL = '{}://{}/v1/token'
+    _API_URL = '{}://{}/v1/token'
     _api_domain = 'oauth.zaif.jp'
 
     def __init__(self, client_id, client_secret):
-        self.__client_id = client_id
-        self.__client_secret = client_secret
+        self._client_id = client_id
+        self._client_secret = client_secret
 
     def get_token(self, code, redirect_uri=None):
         params = {
             'code': code,
-            'client_id': self.__client_id,
-            'client_secret': self.__client_secret,
+            'client_id': self._client_id,
+            'client_secret': self._client_secret,
             'grant_type': 'authorization_code'
         }
         if redirect_uri:
             params['redirect_uri'] = redirect_uri
-        return get_response(self.__API_URL.format(self.get_protocol(), self._api_domain), params)
+        return get_response(self._API_URL.format(self.get_protocol(), self._api_domain), params)
 
     def refresh_token(self, refresh_token):
         params = {
             'refresh_token': refresh_token,
-            'client_id': self.__client_id,
-            'client_secret': self.__client_secret,
+            'client_id': self._client_id,
+            'client_secret': self._client_secret,
             'grant_type': 'refresh_token'
         }
-        return get_response(self.__API_URL, params)
+        return get_response(self._API_URL, params)
