7de9500f8aad9268aa66c048d9bd04f30e6fc1a1
taniguchi_akira <dededededaiou2003@yahoo.co.jp>
2017-02-01 11:13:14 +0900

    バージョン番号カウントアップ

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zaifapi',
-    version='1.2.7',
+    version='1.2.8',
     description='Zaif Api Library',
     long_description='https://pypi.python.org/pypi/zaifapi',
     url='https://github.com/Akira-Taniguchi/zaifapi',
